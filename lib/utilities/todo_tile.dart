b5878677b9ce8afcae780cb6851617a523351a68
SimonMage <75200996+SimonMage@users.noreply.github.com>
2023-10-27 03:51:09 +0200

    Aggiunto pulsante 'tenendo premuto' e vari fix
    - Ora si possono creare attività tenendo premuto il dito per un secondo sullo schermo
    - Sistemati tutti i bug di ottimizzazione e qualche errore di sintassi
    - Aggiornato a Java 8 (Non so perché vedevo Java 7)
    - Rimosse le vecchie attività di default
    - Aggiunta la possibilità di settare giorno e ora di quando far avvenire l'attività
    + Problematiche riscontrate con la l'aggiornamento automatico dei popup, ora viene sempre scritto 'Modifica' nella sezione data del popup
    + La data e l'orario ancora non hanno una chiusura automatica dell'attività raggiunto il tempo prefissato
    - Aggiornato Gradle all'ultima versione, non sembrava funzionarmi
    - Modificato enormemente todo_tile.dart e home_page.dart lasciando abbastanza invariato le funzioni originarie
    - Aggiunti anche qualche colore per i pulsanti per renderli più carini
    
    *___* End of Changelog *___*

---
+++ lib/utilities/todo_tile.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+  final DateTime? taskDate;
+
+  ToDoTile({
+    Key? key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    this.taskDate,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, right: 25, top: 25),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    // Checkbox
+                    Checkbox(value: taskCompleted, onChanged: onChanged),
+                    // Nome lista
+                    Text(taskName),
+                  ],
+                ),
+                if (taskDate != null)
+                  Row(
+                    children: [
+                      //Text("${taskDate!.day}/${taskDate!.month}/${taskDate!.year}"),
+                      Text("${taskDate!.day}/${taskDate!.month}/${taskDate!.year}  ${taskDate!.hour}:${taskDate!.minute}"),
+                    ],
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
